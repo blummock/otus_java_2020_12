af3977d2d560d3f7ba164a7d3121acd6b0c73332
mad7m <mad7mile@gmail.com>
2021-03-17 20:51:47 +0300

    fix

--- L10-byteCodes/src/main/java/TestLogging.java
+++ L10-byteCodes/src/main/java/TestLogging.java
@@ -1,15 +1,18 @@
 public class TestLogging implements TestLoggingInterface {
 
+    @Log
     @Override
     public Integer calculation(Integer param) {
         return 1;
     }
 
+    @Log
     @Override
     public Double calculation(Integer param2, Double param3) {
         return 2.2;
     }
 
+    @Log
     @Override
     public Integer calculation(Integer param1, Integer param2, String param3) {
         return 1;
--- L10-byteCodes/src/main/java/TestLoggingInterface.java
+++ L10-byteCodes/src/main/java/TestLoggingInterface.java
@@ -1,10 +1,9 @@
 public interface TestLoggingInterface {
 
-    @Log
     Integer calculation(Integer param);
-    @Log
+
     Double calculation(Integer param2, Double param3);
-    @Log
+
     Integer calculation(Integer param1, Integer param2, String param3);
 
     Integer calculation(String param);
